82e3e9055f6f44f929a072c1cf541d5e6c8c710a
G-Huynh999 <g.huynh2@share.epsb.ca>
2025-04-01 13:19:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hi");
+document.getElementById("btn").addEventListener("click", placeOrder);
+function placeOrder() {
+  let size = document.getElementById("size-in").value;
+  let topping1 = document.getElementById("topping1-in").value;
+  let topping2 = document.getElementById("topping2-in").value;
+  let msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
+  alert(msg);
+  document.getElementById("output").innerHTML = msg;
+}
